811469cabc3d9f8cec634e38a639b80de10f3890
matan3sh <matan3sh@gmail.com>
2021-06-14 17:16:43 +0300

    Portfolio Resolvers (19)

---
+++ server/data.js
@@ -0,0 +1,28 @@
+const data = {
+  portfolios: [
+    {
+      _id: '32dsa8d9',
+      title: 'Job in USA',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: 'fa6dsa78',
+      title: 'Job in Barcelona',
+      content: 'It was very suny experience',
+      jobTitle: 'Developer',
+      isCurrentlyEmployed: true,
+    },
+    {
+      _id: 'asd7a8d789',
+      title: 'Job in Germany',
+      content: 'It was very good!',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentlyEmployed: true,
+    },
+  ],
+};
+
+module.exports = data;
---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const next = require('next');
+
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const data = require('./data');
+
+app.prepare().then(() => {
+  const server = express();
+
+  const schema = buildSchema(`
+    type Portfolio {
+      _id: ID
+      title: String
+      content: String
+      jobTitle: String
+      daysOfExperience: Int
+      isCurrentlyEmployed: Boolean
+    }
+
+    type Query {
+      hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
+    }
+  `);
+
+  const root = {
+    hello: () => {
+      return 'Hello World';
+    },
+    portfolio: () => {
+      return data.portfolios[0];
+    },
+    portfolios: () => {
+      return data.portfolios;
+    },
+  };
+
+  server.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
